e0d7a2b45ec0b306c98c711444680fd269a8d1f2
pauloparodie <115074891+pauloparodie@users.noreply.github.com>
2022-10-06 20:57:50 +0100

    Update Book.cs

--- DBCodeFirst/DomainModelCF/Book.cs
+++ DBCodeFirst/DomainModelCF/Book.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-
+//ertert
 namespace DBCodeFirst
 {//erterter
     [Table("Book", Schema = "dbo")]
