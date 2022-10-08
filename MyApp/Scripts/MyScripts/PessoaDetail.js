8c8dffcddd07441c5ce01c6494821cae96c64789
Paulo Parodie <pauloparodie@gmail.com>
2022-10-08 02:05:53 +0100

    correcao

--- MyApp/Scripts/MyScripts/PessoaDetail.js
+++ MyApp/Scripts/MyScripts/PessoaDetail.js
@@ -7,7 +7,7 @@
 const SaveDataAsync = async function (data) {
     await $.ajax({
         //url: 'http://www.myapiapp.com:81/Api/Pessoa/Save',
-        url: 'http://myappparodie.azurewebsites.net/Api/Pessoa/Save',
+        url: 'http://myapiappparodie.azurewebsites.net/Api/Pessoa/Save',
         method: 'POST',
         contentType: 'application/json',
         data: data,
