228a9c74ae2e84ac2d14070b076bd71904c8aaf6
Paulo Parodie <pauloparodie@gmail.com>
2022-10-08 20:13:35 +0100

    Test

---
+++ MyApp.Tests/PessoasRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyApp.DomainModel.Entities;
+using MyApp.RepositoryLayer;
+using System.Threading.Tasks;
+using MyApp.RepositoryContract;
+
+
+
+namespace MyApp.Tests
+{
+    [TestClass]
+    public class PessoasRepositoryTest
+    {
+        [TestMethod]
+        public async Task TestCreatePessoa()
+        {
+            using (IPessoasRepositoryManager repo = new PessoasRepositoryManager())
+            {
+
+                Pessoa newPessoa = new Pessoa { Name = "Pessoa Teste", Pais = "Egypt", Age = 23, PhotoName = "pic45.jpg" };
+                try
+                {
+                    await repo.CreatePessoaAsync(newPessoa);
+                }
+                catch (Exception ex)
+                {
+                    int a = 0;
+                }
+                int id = newPessoa.Cod;
+
+                Pessoa pessoaDb = await repo.GetPessoaAsync(newPessoa.Cod);
+                Assert.IsTrue(newPessoa.Name.Equals(pessoaDb.Name));
+                Assert.IsTrue(newPessoa.Pais.Equals(pessoaDb.Pais));
+                Assert.IsTrue(newPessoa.Age.Equals(pessoaDb.Age));
+                Assert.IsTrue(newPessoa.PhotoName.Equals(pessoaDb.PhotoName));
+            }
+        }
+    }
+}
